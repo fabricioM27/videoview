edb24e5f94eae89f13ac47a31deec3d3875e95bf
Usuario <fabriciojaviermoreno3.99@gmail.com>
2023-08-15 18:57:47 -0600

    Initial commit tt

---
+++ app/src/main/java/com/example/videoview/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.videoview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.media.MediaPlayer;
+import android.media.session.MediaController;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "_ao2u7F_Qzg";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+
+
+
+    }
+
+
+
+
+    }
+
